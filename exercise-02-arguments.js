7e551a8913eb92324a295476e757433772a09742
torresomar <torres.omar.eduardo@gmail.com>
2015-12-12 02:15:56 -0600

    Include async

--- exercise-02-arguments.js
+++ exercise-02-arguments.js
@@ -2,6 +2,7 @@ var args = process.argv;
 var length = args.length;
 var sumElements = args.slice(2, length);
 var sum = sumElements.reduce(add, 0);
+console.log(sum);
 
 function add(a, b) {
   a = +a;
--- exercise-04-async.js
+++ exercise-04-async.js
@@ -1,14 +1,10 @@
 var fs = require('fs');
 var filePath = process.argv[2];
 
-function countLines(err, data) {
-  console.log(err, data);
-}
-
 var buff = fs.readFile(filePath, 'utf-8', function(err, data) {
   if(err) {
     throw err;
   }
-
+  console.log(data.toString().split('\n').length - 1);
 });
 
