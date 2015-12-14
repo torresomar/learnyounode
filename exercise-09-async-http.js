1a044c12c7ea6484c6b2ce0a45a6318e80ebae0a
Omar Eduardo <torres.omar.eduardo@gmail.com>
2015-12-14 13:42:01 -0600

    Manage callbacks for http server.

--- exercise-09-async-http.js
+++ exercise-09-async-http.js
@@ -1,12 +1,33 @@
 var bl = require('bl');
-var http =require('http');
+var http = require('http');
 
-http.get(process.argv[2] ,function(response) {
+var urls = process.argv.slice(2, process.argv.length);
+var urlContent = [];
+var waiting = 0;
+
+urls.forEach(function(url, index) {
+  http.get(url ,function(response) {
+    waiting++;
     response.setEncoding('utf8');
-    response.pipe(bl(printBuffer));
+    response.pipe(bl(function(err, data) {
+      urlContent[index] = data.toString();
+    }));
+    response.on('end', function() {
+      waiting--;
+      if (!waiting) {
+        urlContent.map(function(page){console.log(page)});
+      }
+    });
+  });
 });
 
-function printBuffer(err, data) {
-    console.log(data.length);
-    console.log(data.toString());
+
+function printBuffers() {
+  console.log(urlContent);
+  urlContent.forEach(function(url) {
+    console.log(url);
+  });
+  // console.log(urlContent[0]);
+  // console.log(urlContent[1]);
+  // console.log(urlContent[2]);
 }
